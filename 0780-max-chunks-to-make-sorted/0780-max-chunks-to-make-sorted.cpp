5915e990d68f08d6ac6c339ccae3418a4b827660
Nikhil Sahani <121922306+nikhilsenju@users.noreply.github.com>
2024-12-19 10:32:47 +0530

    Time: 0 ms (100%), Space: 9.4 MB (7.32%) - LeetHub

---
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int n = arr.size();
+        int ans = 0;
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            if(sum==i*(i+1)/2){
+                ans++;
+            }
+
+        }
+        return ans;
+
+
+    }
+};
