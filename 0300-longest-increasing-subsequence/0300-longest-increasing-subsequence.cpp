6757ebdf9ba002427272cfd620b8ec8a2b190243
Nikhil Sahani <121922306+nikhilsenju@users.noreply.github.com>
2025-02-09 12:52:32 +0530

    Time: 570 ms (12.88%), Space: 296.6 MB (12.64%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int f(int ind , int prev,vector<int>&nums,vector<vector<int>>&dp){
+        if(ind==nums.size()){
+            return 0;
+        }
+        if(dp[ind][prev+1]!=-1){
+            return dp[ind][prev+1];
+        }
+        int notake = f(ind+1,prev,nums,dp);
+        int take = INT_MIN;
+        if(prev==-1 || nums[ind]>nums[prev]){
+            take =  1+f(ind+1,ind,nums,dp);
+        }
+        return dp[ind][prev+1] = max(take,notake);
+
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        return f(0,-1,nums,dp);
+        
+    }
+};
