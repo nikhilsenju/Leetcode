3731b53434cedb8dfecf16b0bd60b04d127de876
Nikhil Sahani <121922306+nikhilsenju@users.noreply.github.com>
2025-02-18 12:08:31 +0530

    Time: 0 ms (100%), Space: 8.2 MB (49.49%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
@@ -1,47 +1,19 @@
 class Solution {
 public:
-    void f(int ind , int val, string &curr,string &s,vector<string>&v,vector<int>&used){
-        if(ind>=s.size()){
-            v.push_back(curr);
-            return;
-        }
-        if(s[ind]=='I'){
-            for(int j = val+1;j<=9;j++){
-                if(!used[j]){
-                    curr.push_back(j+'0');
-                    used[j]=1;
-                    f(ind+1,j,curr,s,v,used);
-                    curr.pop_back();
-                    used[j]=0;
-                }
-            }
-
-        }
-        else{
-            for(int j = val-1;j>=1;j--){
-                if(!used[j]){
-                    curr.push_back(j+'0');
-                    used[j]=1;
-                    f(ind+1,j,curr,s,v,used);
-                    curr.pop_back();
-                    used[j]=0;
-                }
-            }
-
-        }
-
-    }
     string smallestNumber(string s) {
-        int n  = s.size();
-        vector<string> v;
-        for(int i=1;i<=9;i++){
-                vector<int> used(10,0);
-                used[i]=1;
-                string curr;
-                curr.push_back(i+'0');
-                f(0,i,curr,s,v,used);
+        int n = s.size();
+        stack<int> st;
+        string res;
+        for(int i=0;i<=s.size();i++){
+            st.push(i+1);
+            if(i==n || s[i]=='I'){
+                while(!st.empty()){
+                    res.push_back(st.top()+'0');
+                    st.pop();
+                }
+            }
         }
-        sort(v.begin(),v.end());
-        return v[0];
+        return res;
+        
     }
 };
