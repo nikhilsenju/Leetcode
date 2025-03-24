be1bbe75b1f4c7a44716a354480c9753d22e265c
Nikhil Sahani <121922306+nikhilsenju@users.noreply.github.com>
2025-03-24 17:17:52 +0530

    Time: 22 ms (10.91%), Space: 40.3 MB (12.13%) - LeetHub

--- 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
@@ -1,39 +1,47 @@
 class Solution {
 public:
-    long long countPaths(int n, vector<vector<int>>& roads) {
-        vector<pair<long long,long long>> adj[n];
-        for(auto i:roads){
-            adj[i[0]].push_back({i[1],i[2]});
-            adj[i[1]].push_back({i[0],i[2]});
-            
+    long long mod=1e9+7;
+    int countPaths(int n, vector<vector<int>>& v) {
+      
+        //buid graph
+        vector<vector<pair<long long,long long>>>adj(n);
+        for(auto i:v){
+            long long u = i[0];
+            long long v = i[1];
+            long long wt = i[2];
+            adj[u].push_back({v,wt});
+            adj[v].push_back({u,wt});
         }
-        vector<long long> dist(n,LONG_MAX),ways(n,0);
-        ways[0]=1;
-        priority_queue<pair<long long,long long>,vector<pair<long long,long long>>,greater<pair<long long,long long>>> pq;
+        //algorithm 
+        priority_queue<pair<long long,long long>,vector<pair<long long,long long>>,greater<pair<long long,long long>>>pq;
         pq.push({0,0});
-        dist[0]=0;
-        long long mod=(int)1e9+7;
+        vector<long long> dis(n,1e18);
+        dis[0]=0;
+        vector<long long>count(n,0);
+        count[0]=1;
         while(!pq.empty()){
-            long long node,dis;
-            dis=pq.top().first;
-            node=pq.top().second;
+            auto it = pq.top();
             pq.pop();
+            long long curdis = it.first;
+            long long node = it.second;
+            if(curdis!=dis[node]){
+                continue;
+            }
             for(auto i:adj[node]){
-                long long edw,adjnode;
-                adjnode=i.first;
-                edw=i.second;
-                if(dis+edw<dist[adjnode]){
-                    dist[adjnode]=dis+edw;
-                    pq.push({dis+edw,adjnode});
-                    ways[adjnode]=ways[node]%mod;
+                long long ngh = i.first;
+                long long wt = i.second;
+                if(dis[ngh]>dis[node]+wt){
+                    dis[ngh]=dis[node]+wt;
+                    pq.push({dis[ngh],ngh});
+                    count[ngh]=(count[node])%mod;
                 }
-                else if(dis+edw==dist[adjnode]){
-                    ways[adjnode]=(ways[adjnode]+ways[node])%mod;
+                else if(dis[ngh]==dis[node]+wt){
+                    count[ngh]=(count[node]+count[ngh])%mod;
                 }
             }
-
         }
-        return ways[n-1]%mod;
+        return count[n-1]%mod;
+
 
         
     }
