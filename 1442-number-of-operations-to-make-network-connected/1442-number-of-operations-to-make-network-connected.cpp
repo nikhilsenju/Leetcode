f19b8c8f26bc10049f5a748edfa071864f387db0
Nikhil Sahani <121922306+nikhilsenju@users.noreply.github.com>
2025-03-24 21:47:56 +0530

    Time: 21 ms (52.16%), Space: 47.7 MB (31.36%) - LeetHub

---
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,74 @@
+class dsu {
+public:
+    vector<long long> parent, size;
+    long long Number_of_Nodes, Number_of_Groups, Max_Group;
+
+    dsu(long long Number_of_Nodes) : Number_of_Nodes(Number_of_Nodes), Number_of_Groups(Number_of_Nodes), Max_Group(1) {
+        parent.resize(Number_of_Nodes + 1);
+        size.resize(Number_of_Nodes + 1, 1);
+        for (long long i = 1; i <= Number_of_Nodes; ++i) {
+            parent[i] = i;
+        }
+    }
+
+    long long up(long long x) {
+        if (parent[x] != x) {
+            parent[x] = up(parent[x]);
+        }
+        return parent[x];
+    }
+
+    bool is_same_Group(long long x, long long y) {
+        return up(x) == up(y);
+    }
+
+    void unionbysize(long long x, long long y) {
+        long long leader1 = up(x);
+        long long leader2 = up(y);
+        if (leader1 != leader2) {
+            Number_of_Groups--;
+            if (size[leader1] < size[leader2]) {
+                swap(leader1, leader2);
+            }
+            parent[leader2] = leader1;
+            size[leader1] += size[leader2];
+            Max_Group = max(Max_Group, size[leader1]);
+        }
+    }
+
+    long long getsize(long long x) {
+        return size[up(x)];
+    }
+};
+
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& v) {
+        dsu ds(n);
+        int tot = 0;
+        for(auto i:v){
+            int u = i[0];
+            int v = i[1];
+            if(!ds.is_same_Group(u,v)){
+                ds.unionbysize(u,v);
+            }
+            else{
+                tot++;
+            }
+        }
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            if(i==ds.up(i)){
+                c++;
+            }
+            
+        }
+        if(c-1>tot){
+            return -1;
+        }
+        return c-1;
+
+        
+    }
+};
