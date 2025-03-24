a35827e72a66cf7009846648a6f270763880a4f9
Nikhil Sahani <121922306+nikhilsenju@users.noreply.github.com>
2025-03-24 07:04:52 +0530

    Time: 30 ms (39.7%), Space: 23.6 MB (79.61%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -3,38 +3,39 @@ public:
     struct inf{
         int x,y,d;
         inf(int x,int y,int d):x(x),y(y),d(d){}
+        struct compare{
+            bool operator()(inf &a ,inf &b){
+                return a.d>b.d;
+            }
+        };
     };
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        if(grid[0][0]!=0){
-            return -1;
-        }
-        int n =grid.size();
-        queue<inf> q;
-        vector<vector<int>>vis(n,vector<int>(n,0));
-        q.push(inf(0,0,1));
-        vis[0][0]=1;
-        while(!q.empty()){
-            auto it = q.front();
-            q.pop();
+        if(grid[0][0]==1){return -1;}
+        int n = grid.size();
+        vector<vector<int>>dis(n,vector<int>(n,1e9));
+        priority_queue<inf,vector<inf>,inf :: compare>pq;
+        pq.push(inf(0,0,1));
+        dis[0][0]=1;
+        while(!pq.empty()){
+            auto it = pq.top();
+            pq.pop();
             int x = it.x;
             int y = it.y;
             int d = it.d;
-            if(x==n-1 && y==n-1){
-                return d;
-            }
             for(int i=-1;i<=1;i++){
                 for(int j=-1;j<=1;j++){
                     int nx = x+i;
                     int ny = y+j;
-                    if(nx>=0 && ny>=0 && nx<n && ny<n && !vis[nx][ny] && grid[nx][ny]==0){
-                        q.push(inf(nx,ny,d+1));
-                        vis[nx][ny]=1;
+                    if(nx>=0 && nx<n && ny>=0 && ny<n && grid[nx][ny]==0 &&(dis[x][y]+1<dis[nx][ny])){
+                        dis[nx][ny]=1+dis[x][y];
+                        pq.push(inf(nx,ny,dis[nx][ny]));
                     }
-
                 }
             }
         }
-        return -1;
+        if(dis[n-1][n-1]==1e9){return -1;}
+        return dis[n-1][n-1];
+        
         
     }
 };
