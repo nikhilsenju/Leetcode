62fd699020b19ef09a3bdc36b8a7a3482bcc8f10
Nikhil Sahani <121922306+nikhilsenju@users.noreply.github.com>
2025-05-19 12:59:46 +0530

    Time: 4 ms (18.73%), Space: 17.5 MB (13.85%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,55 +1,57 @@
 class Solution {
 public:
-  struct Pair{
+    struct Node {
         int x;
         int y;
         int time;
-        Pair(int x,int y,int time):x(x),y(y),time(time){}
+        Node(int x, int y, int time) : x(x), y(y), time(time) {};
     };
+
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int maxTime = 0;
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        queue<Pair> q;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
-                    q.push(Pair(i,j,0));
-                    vis[i][j]=1;
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+        queue<Node> q;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 2) {
+                    q.push(Node(i, j, 0));
+                    vis[i][j] = 1;
+                    dist[i][j] = 0;
                 }
             }
         }
-        vector<int> row = {0,1,0,-1};
-        vector<int> col = {1,0,-1,0};
-        while(!q.empty()){
-            Pair p = q.front();
+        int minTime = 0;
+        vector<int> dx = {-1, 0, 1, 0};
+        vector<int> dy = {0, 1, 0, -1};
+        while (!q.empty()) {
+            auto it = q.front();
             q.pop();
-            int x = p.x;
-            int y = p.y;
-            int time = p.time;
-            maxTime = max(maxTime,time);
-            for(int i=0;i<4;i++){
-                int nr = x+row[i];
-                int nc = y+col[i];
-                if(nr>=0 && nr<n && nc>=0 && nc<m && (!vis[nr][nc]) && grid[nr][nc]==1){
-                    q.push(Pair(nr,nc,time+1));
-                    vis[nr][nc]=1;
-                    grid[nr][nc]=2;
+            int x = it.x;
+            int y = it.y;
+            int time = it.time;
+            minTime = max(minTime, time);
+            for (int k = 0; k < 4; k++) {
+                int nx = x + dx[k];
+                int ny = y + dy[k];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && !vis[nx][ny] &&
+                    grid[nx][ny] == 1 && 1 + time < dist[nx][ny]) {
+                    grid[nx][ny] = 2;
+                    q.push(Node(nx, ny, time + 1));
+                    vis[nx][ny] = 1;
+                    dist[nx][ny] = time + 1;
                 }
             }
-            
-
-
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
                     return -1;
                 }
             }
         }
-        return maxTime;
-        
+        return minTime;
     }
 };
