0998dead5496b6e9eb5c6cf9742650536ff627e3
Nikhil Sahani <121922306+nikhilsenju@users.noreply.github.com>
2025-05-19 13:29:21 +0530

    Time: 58 ms (69.55%), Space: 21.1 MB (63.69%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
-    int ladderLength(string bw, string ew, vector<string>& wl) {
-        set<string> st(wl.begin(),wl.end());
-        queue<pair<string,int>> q;
-        q.push({bw,1});
-        while(!q.empty()){
+    struct Node {
+        string s;
+        int steps;
+        Node(string s, int steps) : s(s), steps(steps) {};
+    };
+    int ladderLength(string st, string end ,vector<string>& v) {
+        unordered_set<string> used(v.begin(), v.end());
+        queue<Node> q;
+        q.push(Node(st, 1));
+        if (used.count(st)) {
+            used.erase(st);
+        }
+        while (!q.empty()) {
             auto it = q.front();
             q.pop();
-            string word = it.first;
-            int level = it.second;
-            if(word==ew){
-                return level;
+            string curr = it.s;
+            int steps = it.steps;
+            if (curr == end) {
+                return steps;
             }
-            for(int i=0;i<word.size();i++){
-                string tmp=word;
-                for(char c = 'a';c<='z';c++){
-                    tmp[i]=c;
-                    if(st.count(tmp)){
-                        q.push({tmp,level+1});
-                        st.erase(tmp);
-                    }   
+            for (int i = 0; i < curr.size(); i++) {
+                string tmp = curr;
+                for (char c = 'a'; c <= 'z'; c++) {
+                    tmp[i] = c;
+                    if (used.find(tmp)!=used.end()){
+                            used.erase(tmp);
+                            q.push(Node(tmp, steps + 1));
+                        }
                 }
             }
         }
         return 0;
-        
     }
 };
