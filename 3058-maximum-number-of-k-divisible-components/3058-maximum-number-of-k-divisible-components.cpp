fa37982aaa343a3b952190f4339a0bde4dcda960
Nikhil Sahani <121922306+nikhilsenju@users.noreply.github.com>
2025-05-26 18:14:54 +0530

    Time: 219 ms (13.44%), Space: 191 MB (14%) - LeetHub

--- 3058-maximum-number-of-k-divisible-components/3058-maximum-number-of-k-divisible-components.cpp
+++ 3058-maximum-number-of-k-divisible-components/3058-maximum-number-of-k-divisible-components.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
+
     int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
-        vector<vector<int>> adj(n+1);
+        vector<vector<int>> adj(n);
         for(auto i:edges){
-            adj[i[0]].push_back(i[1]);
-            adj[i[1]].push_back(i[0]);
+            int u = i[0];
+            int v = i[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
         int ans = 0;
-        vector<long long> v(n,0); 
-        function<long long(int,int)>dfs = [&](int node , int parent){
-            v[node]=0;
-            for(auto it:adj[node]){
-                if(it!=parent){
-                    v[node]+=dfs(it,node);
+        function<long long(int,int)>dfs=[&](int node ,int par){
+            long long sum = 0;
+            for(auto i:adj[node]){
+                if(i!=par){
+                    sum+=dfs(i,node);
                 }
             }
-            v[node]+=values[node];
-            if(v[node]%k==0){ans++;return (long long)0;}
-            return v[node];
+            sum+=values[node];
+            if(sum%k==0){
+                ans++;
+                return (long long)0;
+            }
+            return sum;
         };
         dfs(0,-1);
         return ans;
-
-
 
         
     }
