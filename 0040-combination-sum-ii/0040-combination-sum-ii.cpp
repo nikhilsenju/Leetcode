fb2f21f2b8f1cae69371c4fedf64f935c076fd24
Nikhil Sahani <121922306+nikhilsenju@users.noreply.github.com>
2025-06-17 12:03:39 +0530

    Time: 0 ms (100%), Space: 14.1 MB (60.88%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
-    void f(int ind, int tar, vector<int>& tmp, vector<int>& v, vector<vector<int>>& ans) {
-        if (tar == 0) { // Base condition
-            ans.push_back(tmp);
-            return;
-        }
+    void solve(int ind ,int tar,  vector<int>&v,vector<int>&tmp,vector<vector<int>>&ans){
+       
+            if(tar==0){
+                ans.push_back(tmp);
+                return;
+            }
 
-        for (int i = ind; i < v.size(); i++) {
-            if (i > ind && v[i] == v[i - 1]) continue; // Skip duplicates
-            if (v[i] > tar) break; // Stop if current element is greater than target
-            
+        
+        for(int i=ind;i<v.size();i++){
+            if(i>ind && v[i-1]==v[i]){
+                continue;
+            } 
+            if(tar<v[i]){
+                break;
+            }
             tmp.push_back(v[i]);
-            f(i + 1, tar - v[i], tmp, v, ans);
+            solve(i+1,tar-v[i],v,tmp,ans);
             tmp.pop_back();
         }
-    }
 
-    vector<vector<int>> combinationSum2(vector<int>& v, int k) {
-        sort(v.begin(), v.end());
+    }
+    vector<vector<int>> combinationSum2(vector<int>& c, int t) {
+        sort(c.begin(),c.end());
         vector<vector<int>> ans;
         vector<int> tmp;
-        f(0, k, tmp, v, ans);
+        solve(0,t,c,tmp,ans);
         return ans;
     }
 };
