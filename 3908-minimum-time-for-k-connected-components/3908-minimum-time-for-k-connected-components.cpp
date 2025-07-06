360e6a6b82d769b3a4877bed0686e7c67f344515
Nikhil Sahani <121922306+nikhilsenju@users.noreply.github.com>
2025-07-06 21:04:26 +0530

    Time: 175 ms (69.38%), Space: 406.8 MB (31.38%) - LeetHub

--- 3908-minimum-time-for-k-connected-components/3908-minimum-time-for-k-connected-components.cpp
+++ 3908-minimum-time-for-k-connected-components/3908-minimum-time-for-k-connected-components.cpp
@@ -1,34 +1,44 @@
 class dsu {
 public:
-    vector<int> parent, size;
-    int Number_of_Groups;
+    vector<long long> parent, size;
+    long long Number_of_Groups;
 
-    dsu(int n) : parent(n), size(n, 1), Number_of_Groups(n) {
-        iota(parent.begin(), parent.end(), 0);
+    dsu(long long n) : Number_of_Groups(n) {
+        parent.resize(n);
+        size.resize(n, 1);
+        for (long long i = 0; i < n; ++i) {
+            parent[i] = i;
+        }
     }
 
-    int find(int x) {
-        return parent[x] == x ? x : parent[x] = find(parent[x]);
+    long long up(long long x) {
+        if (parent[x] != x) {
+            parent[x] = up(parent[x]);
+        }
+        return parent[x];
     }
 
-    void unionbysize(int x, int y) {
-        int lx = find(x);
-        int ly = find(y);
-        if (lx == ly) return;
-        if (size[lx] < size[ly]) swap(lx, ly);
-        parent[ly] = lx;
-        size[lx] += size[ly];
-        Number_of_Groups--;
+    void unionbysize(long long x, long long y) {
+        long long leader1 = up(x);
+        long long leader2 = up(y);
+        if (leader1 != leader2) {
+            Number_of_Groups--;
+            if (size[leader1] < size[leader2]) {
+                swap(leader1, leader2);
+            }
+            parent[leader2] = leader1;
+            size[leader1] += size[leader2];
+        }
     }
 };
 
 class Solution {
 public:
-    bool atleastkwithintime(int mid, int k, vector<vector<int>>& edges, int n) {
+    bool atleastkwithintime(int mid, int k, vector<vector<int>>& adj, int n) {
         dsu ds(n);
-        for (const auto& e : edges) {
-            int u = e[0], v = e[1], t = e[2];
-            if (t > mid) {
+        for (auto& i : adj) {
+            int u = i[0], v = i[1], t = i[2];
+            if (mid < t) {
                 ds.unionbysize(u, v);
             }
         }
@@ -37,7 +47,7 @@ public:
 
     int minTime(int n, vector<vector<int>>& edges, int k) {
         int lo = 0, hi = 1e9, ans = -1;
-        while (lo <= hi) {
+        while (hi >= lo) {
             int mid = lo + (hi - lo) / 2;
             if (atleastkwithintime(mid, k, edges, n)) {
                 ans = mid;
