fbcc5cc7557ae654a5ae1227079e802930f7434f
Nikhil Sahani <121922306+nikhilsenju@users.noreply.github.com>
2025-07-10 21:53:01 +0530

    Time: 214 ms (5.48%), Space: 174.9 MB (6.85%) - LeetHub

--- 2893-visit-array-positions-to-maximize-score/2893-visit-array-positions-to-maximize-score.cpp
+++ 2893-visit-array-positions-to-maximize-score/2893-visit-array-positions-to-maximize-score.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    unordered_map<long long, long long> dp; // key = (index << 1) | parity
-
-    long long solve(int ind, int p, vector<int>& v, int x) {
-        if (ind >= v.size()) return 0;
-
-        long long key = ((long long)ind << 1) | p;
-        if (dp.count(key)) return dp[key];
-
-        int cur = ((v[ind] % 2) == p) ? v[ind] : 0;
-        long long change = -x + solve(ind + 1, !p, v, x) + cur;
-        long long nott = solve(ind + 1, p, v, x) + cur;
-
-        return dp[key] = max(change, nott);
+    long long solve(long long ind ,long long p ,vector<int>&v,long long x,vector<vector<long long>>&dp){
+        if(ind>=v.size()){
+            return 0;
+        }
+        if(dp[ind][p]!=-1){
+            return dp[ind][p];
+        }
+        long long cur = (v[ind]%2)==(p)?v[ind]:0;
+        long long change = -x + solve(ind+1,!p,v,x,dp)+cur;
+        long long nott = solve(ind+1,p,v,x,dp)+cur;
+        return dp[ind][p]=max(change,nott);
     }
-
     long long maxScore(vector<int>& nums, int x) {
-        return solve(0, nums[0] % 2, nums, x);
+        long long n = nums.size();
+        vector<vector<long long>>dp(n+1,vector<long long>(2,-1LL));
+        return solve(0,nums[0]%2,nums,x,dp);
+
+        
     }
 };
