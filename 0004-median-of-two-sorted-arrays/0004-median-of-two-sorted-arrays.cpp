82708f9a52a55a012bb738ef64262de199a25ba8
Nikhil Sahani <121922306+nikhilsenju@users.noreply.github.com>
2025-08-14 12:29:23 +0530

    Time: 0 ms (100%), Space: 95 MB (79.23%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,49 +1,48 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        long long n1 = nums1.size();
-        long long n2 = nums2.size();
+        int n1 = nums1.size();
+        int n2 = nums2.size();
         if(n1>n2){
-            swap(nums1,nums2);
-            swap(n1,n2);
+            return findMedianSortedArrays(nums2,nums1);
         }
-        long long lo = 0;
-        long long hi = nums1.size();
-        long long left = (n1+n2+1)/2;
-        long long n = n1+n2;
+        int tot = n1+n2;
+        int left = (tot+1)/2;
+        int lo = 0;
+        int hi = n1;
         while(hi>=lo){
-            long long mid = (hi+lo)>>1;
-            long long mid1 = mid;
-            long long mid2 = left - mid1;
-            long long l1 = INT_MIN;
-            long long l2 = INT_MIN;
-            long long r1 = INT_MAX;
-            long long r2 = INT_MAX;
-            if(mid1-1>=0){
-                l1 = nums1[mid1-1];
-            }
-            if(mid2-1>=0){
-                l2 = nums2[mid2-1];
-            }
+            int mid1 = (lo+hi)>>1;
+            int mid2 = left - mid1;
+            int l1 = INT_MIN;
+            int l2 = INT_MIN;
+            int r1 = INT_MAX;
+            int r2 = INT_MAX;
             if(mid1<n1){
                 r1 = nums1[mid1];
             }
             if(mid2<n2){
                 r2 = nums2[mid2];
             }
+            if(mid1-1>=0){
+                l1 = nums1[mid1-1];
+            }
+            if(mid2-1>=0){
+                l2 = nums2[mid2-1];
+            }
             if(l1<=r2 && l2<=r1){
-                if(n%2){
+                if(tot%2){
                     return double(max(l1,l2));
                 }
-                return (double)((max(l1,l2)+min(r1,r2))/2.0);
+                return double(max(l1,l2)+min(r1,r2))/2.0;
             }
-            else if(l1>r2){
-                hi = mid-1;
+            if(l1>r2){
+                hi = mid1-1;
             }
             else{
-                lo = mid+1;
+                lo = mid1+1;
             }
+
         }
-        return 0;
+        return -1;
     }
 };
