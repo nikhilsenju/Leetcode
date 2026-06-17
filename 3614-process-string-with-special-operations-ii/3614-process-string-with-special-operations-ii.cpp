731c9facbf7e5ee98ce6adfde4bce217b4dd3acb
Nikhil Sahani <121922306+nikhilsenju@users.noreply.github.com>
2026-06-17 13:22:38 +0530

    Time: 21 ms (81.61%), Space: 24.1 MB (28.74%) - LeetHub

---
+++ 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    char processStr(string s, long long k) {
+        long long len = 0;
+        for(auto i:s){
+            if(i=='*'){
+                if(len){
+                    len--;
+                }
+            }
+            else if(i=='%'){
+                continue;
+            }
+            else if(i=='#'){
+                len*=2;
+            }
+            else{
+                len++;
+            }
+        }
+        if(k+1>len){
+            return '.';
+        }
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]=='*'){
+                len++;
+            }
+            else if(s[i]=='#'){
+                if(k+1>(len+1)/2){
+                    k-=len/2;
+                }
+                    len=(len+1)/2;
+            }
+            else if(s[i]=='%'){
+                k=len-k-1;
+            }
+            else{
+                if(k+1==len){
+                    return s[i];
+                }
+                else{
+                    len--;
+                }
+
+            }
+        }
+        return '.';
+    
+
+    }
+};
