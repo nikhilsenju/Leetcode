681973272c8130855983697d28726a7ad8bb7d1b
Nikhil Sahani <121922306+nikhilsenju@users.noreply.github.com>
2026-07-14 11:55:25 +0530

    Time: 1122 ms (47.82%), Space: 60.3 MB (60%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -4,16 +4,8 @@ public:
     int dp[205][205][205];
     int n;
 
-    int gcd(int a, int b) {
-        while (b) {
-            int t = a % b;
-            a = b;
-            b = t;
-        }
-        return a;
-    }
-
     int solve(int ind, int gcd1, int gcd2, vector<int>& nums) {
+        // Base case
         if (ind == n) {
             if (gcd1 == 0 || gcd2 == 0)
                 return 0;
@@ -25,12 +17,15 @@ public:
 
         long long ans = 0;
 
-        ans += solve(ind + 1, gcd(gcd1, nums[ind]), gcd2, nums);
+        // Put nums[ind] in first subsequence
+        ans += solve(ind + 1, std::gcd(gcd1, nums[ind]), gcd2, nums);
         ans %= MOD;
 
-        ans += solve(ind + 1, gcd1, gcd(gcd2, nums[ind]), nums);
+        // Put nums[ind] in second subsequence
+        ans += solve(ind + 1, gcd1, std::gcd(gcd2, nums[ind]), nums);
         ans %= MOD;
 
+        // Skip nums[ind]
         ans += solve(ind + 1, gcd1, gcd2, nums);
         ans %= MOD;
 
